0ba922e53af1f2b4e3873c4faabe43c672c94885
xuandien24111997 <hoangxuandien24111997@gmail.com>
2021-05-17 09:46:55 +0700

    first commit

---
+++ src/App/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './layouts/Header'
+import Footer from './layouts/Footer'
+
+function HomePage() {
+  return (
+    <div className="home">
+      <Header />
+      <h1>Home Page</h1>
+      <Footer />
+    </div>
+  );
+}
+
+export default HomePage;
