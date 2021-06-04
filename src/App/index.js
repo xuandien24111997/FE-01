40c5c2110389583d59240532d9d1a25576afab8d
xuandien24111997 <hoangxuandien24111997@gmail.com>
2021-06-04 10:29:08 +0700

    v1.2

--- src/App/index.js
+++ src/App/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import Home from './page/Home'
 import Products from "./page/Products";
 import ProductDetail from "./page/ProductDetail";
